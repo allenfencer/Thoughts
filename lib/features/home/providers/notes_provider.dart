7045dcbc87bd1b2a836b13f5c4c3e0d9b93f2b1c
allenfencer <allenfencer316@gmail.com>
2024-05-25 16:56:50 +0400

    minor bug fix

--- lib/features/home/providers/notes_provider.dart
+++ lib/features/home/providers/notes_provider.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:thoughts/features/auth/services/auth_service.dart';
@@ -6,7 +5,7 @@ import 'package:thoughts/features/auth/services/auth_service.dart';
 final notesStream = Provider((ref) => Supabase.instance.client
     .from('Journals')
     .stream(primaryKey: ['id']).inFilter(
-        'user_id', [ref.watch(supabaseProvider).auth.currentUser!.id]));
+        'user_id', [ref.read(supabaseProvider).auth.currentUser!.id]));
 
 final journalProvider = StreamProvider<List<Map<String, dynamic>>>((ref) {
   final journalData = ref.watch(notesStream);
